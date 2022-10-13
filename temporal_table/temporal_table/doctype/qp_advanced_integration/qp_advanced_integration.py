d569f9e3400781d850857f6e2f335939404a46c0
afuentes-alphas <aryrosa.fuentes@alphas.technology>
2022-10-12 20:01:55 -0400

    [MOD] Se incluye descuentos y validaciones.

--- temporal_table/temporal_table/doctype/qp_advanced_integration/qp_advanced_integration.py
+++ temporal_table/temporal_table/doctype/qp_advanced_integration/qp_advanced_integration.py
@@ -380,7 +380,7 @@ def load_sales_order(doc):
 
 	validation_result, validation_msg = validate_so2save(doc.name)
 
-	if validation_result == False:
+	if validation_result:
 
 		raise Exception("Validation of required fields: {}".format(validation_msg))
 
@@ -416,6 +416,8 @@ def load_sales_order(doc):
 
 		item_currency = ""
 
+		item_shipping_address = ""
+
 		for item in item_data:
 
 			# Se asume que es un archivo por cliente
@@ -426,6 +428,10 @@ def load_sales_order(doc):
 			if not item_currency:
 
 				item_currency = item.get("currency") or ""
+			
+			if not item_shipping_address:
+
+				item_shipping_address = item.get("shipping_address") or ""
 
 			prod_id = item.get('product')
 
@@ -437,31 +443,35 @@ def load_sales_order(doc):
 			uom_from_list = __get_uom_from_list(prod_id, prod_uom)
 			item_uom_conv = uom_from_list and uom_from_list[0]['conversion_factor'] or 1
 
-			company_abbr = frappe.db.get_value("Company", doc.company, "abbr")
+			company_abbr = frappe.db.get_value("Company", so_header.get('company'), "abbr")
 
-			item_disc = item.get('discount')
-			item_amount = item.get('price')
+			item_disc = item.get('discount') or 0
 
-			# TODO: Validar y Registrar dirección de envío
-			# TODO: Validar categoria -  producto - uom - compañía - cantidad > 0 etc
-			# Incorporar descuento
-			# "discount_percentage": item.get('discount'),
-			# "discount_amount": flt(item_amount) - (flt(item_amount) * flt(item_disc)/100)
-			# para descuento se debe verificar que otro campo se debe registrar el descuento
+			# TODO: Definir de donde tomar el precio en caso de no existir en el archivo
+			# La solución aplicada asume que se antiene el proceso de actualización de precios de VF
+			price_list = frappe.db.get_single_value("Selling Settings", "selling_price_list")
+			price_default = frappe.get_list('Item Price', filters={'item_code': prod_id, 'price_list': price_list}, fields=['price_list_rate'])
+			price_default = price_default and price_default[0]['price_list_rate'] or 0
+			item_amount = item.get('price') or price_default
+
+			# TODO: Validar cantidad > 0 
+
 			order_items.append({
 				"item_code": prod_id,
 				"item_name": prod_name,
 				"description": 'PROD: {0} - CAT: {1}'.format(prod_id, so_header.get('category')),
-				"rate": item_amount,
+				"rate": flt(item_amount) - (flt(item_amount) * flt(item_disc)/100),
 				"qty": item.get('product_qty'),
 				"stock_uom": prod_uom,
+				"price_list_rate": item_amount,
+				"margin_type": 'Percentage',
+				"discount_percentage": item.get('discount'),
+				"discount_amount": flt(item_amount) * flt(item_disc)/100,
 				"conversion_factor": item_uom_conv,
 				"delivery_date": delivery_date,
 				"warehouse": "{} - {}".format(item.get('store'), company_abbr),
 			})
 
-		# Se configura por defecto en la orden de venta, así que se omite:
-		# "naming_series": 'SO-.{qp_year_week}.-.#######',
 		obj_data = {
 			"company": so_header.get('company'),
 			"customer": item_customer,
@@ -476,7 +486,11 @@ def load_sales_order(doc):
 			"doctype": "Sales Order"
 		}
 
-		print("obj_data--->", obj_data)
+		if item_shipping_address:
+
+			obj_data['shipping_address_name'] = item_shipping_address
+
+		# print("obj_data--->", obj_data)
 
 		sale_order = frappe.get_doc(obj_data)
 
@@ -489,6 +503,19 @@ def validate_so2save(doc_name):
 
 	# Validar campos
 
+	if __group_by_currency(doc_name):
+
+		msg_res += _("There is different currency for a document or there is no currency\n")
+
+	if __group_by_shipping_address(doc_name):
+
+		msg_res += _("There is different shipping address for a document or there is no shipping address\n")
+
+	return msg_res and True or False, msg_res
+
+
+def __group_by_currency(doc_name):
+
 	# Validar que sea un mismo tipo de moneda por sales order a crear
 	sql_str = """
 		Select count(currency) as curr from (
@@ -500,16 +527,43 @@ def validate_so2save(doc_name):
 		group by company, category, reference_1, year_week
 		having curr > 1
 	""".format(origin_process=doc_name)
+	res = frappe.db.sql(sql_str, as_dict=1)
 
-	data = frappe.db.sql(sql_str, as_dict=1)
+	return res and True or False
 
-	print("data", data)
 
-	if data:
+def __group_by_shipping_address(doc_name):
 
-		msg_res = _("There is different currency for a document")
+	# Validar que sea un mismo tipo de dirección de envío por sales order a crear
+	sql_str = """
+		Select count(shipping_address) as ship_addr from (
+			select company, category, reference_1, year_week, shipping_address
+			from tabqp_tmp_sales_orders
+			where origin_process = '{origin_process}'
+			group by  company, category, reference_1, year_week, shipping_address 
+		) as dbtbl
+		group by company, category, reference_1, year_week
+		having ship_addr > 1
+	""".format(origin_process=doc_name)
+	res = frappe.db.sql(sql_str, as_dict=1)
 
-	return True, msg_res
+	result = res and True or False
+
+	# Validar que exista la dirección de envío en la BD
+	if not result:
+
+		sql_str = """
+			select tso.shipping_address, addr.name
+			from tabqp_tmp_sales_orders as tso
+			left outer join tabAddress as addr on tso.shipping_address = addr.name
+			where tso.origin_process = '{origin_process}' and addr.name is Null and tso.shipping_address is not Null
+			group by  tso.company, tso.category, tso.reference_1, tso.year_week, tso.shipping_address
+		""".format(origin_process=doc_name)
+		res = frappe.db.sql(sql_str, as_dict=1)
+
+		result = res and True or False
+
+	return result
 
 
 def __get_uom_from_list(item_name, item_uom):
